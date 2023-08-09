0bb3f73450e20c86ab82c8b790243e8aa3cec242
Temiloluwa Adewale <98469754+stylebytemi@users.noreply.github.com>
2023-08-09 03:53:12 +0100

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,21 +5,26 @@ the top ten hot posts of a subreddit
 """
 import re
 import requests
+import sys
 
 def add_title(dictionary, hot_posts):
-    if not hot_posts:
+    """ Adds item into a list """
+    if len(hot_posts) == 0:
         return
 
     title = hot_posts[0]['data']['title'].split()
     for word in title:
+        word_lower = word.lower()
         for key in dictionary.keys():
-            c = re.compile(r"\b{}\b".format(re.escape(key)), re.I)
-            if c.findall(word):
+            key_lower = key.lower()
+            c = re.compile(r"\b{}\b".format(re.escape(key_lower)), re.I)
+            if c.findall(word_lower):
                 dictionary[key] += 1
     hot_posts.pop(0)
     add_title(dictionary, hot_posts)
 
 def recurse(subreddit, dictionary, after=None):
+    """ Queries to Reddit API """
     u_agent = 'Mozilla/5.0'
     headers = {
         'User-Agent': u_agent
@@ -36,27 +41,35 @@ def recurse(subreddit, dictionary, after=None):
                        allow_redirects=False)
 
     if res.status_code != 200:
-        return
+        return None
 
     dic = res.json()
     hot_posts = dic['data']['children']
     add_title(dictionary, hot_posts)
     after = dic['data']['after']
-    if after:
-        recurse(subreddit, dictionary, after=after)
+    if not after:
+        return
+    recurse(subreddit, dictionary, after=after)
 
 def count_words(subreddit, word_list):
-    dictionary = {word: 0 for word in word_list}
+    """ Init function """
+    dictionary = {}
+
+    for word in word_list:
+        dictionary[word] = 0
 
     recurse(subreddit, dictionary)
 
-    sorted_words = sorted(dictionary.items(), key=lambda kv: (-kv[1], kv[0]))
+    l = sorted(dictionary.items(), key=lambda kv: (-kv[1], kv[0]))
 
-    for word, count in sorted_words:
-        if count != 0:
-            print("{}: {}".format(word.lower(), count))
+    if len(l) != 0:
+        for item in l:
+            if item[1] != 0:
+                print("{}: {}".format(item[0], item[1]))
+    else:
+        print("")
 
 # Example usage
-# subreddit = 'python'
-# word_list = ['python', 'javascript', 'java']
-# count_words(subreddit, word_list)
+subreddit = 'python'
+word_list = ['python', 'javascript', 'java']
+count_words(subreddit, word_list)
